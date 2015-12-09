75a5237be3714c3f5a2cf1e7534ec58075888818
Cajova-Houba <tkmushroomer@gmail.com>
2015-12-09 08:39:58 +0200

    Stable, build with mvn install

---
+++ src/main/java/org/microapp/dao/hib/base/SearchException.java
@@ -0,0 +1,13 @@
+package org.microapp.dao.hib.base;
+
+/**
+*
+* @author jgarcia
+*/
+public class SearchException extends RuntimeException {
+
+   public SearchException(Throwable ex) {
+       super(ex);
+   }
+
+}
